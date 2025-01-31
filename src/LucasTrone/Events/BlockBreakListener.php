b301026094ffa5e3bae57bd40818b224c70d7ee0
luuuccasss <algelucaspro@gmail.com>
2025-01-31 21:52:41 +0100

    Added debugs

--- src/LucasTrone/Events/BlockBreakListener.php
+++ src/LucasTrone/Events/BlockBreakListener.php
@@ -9,7 +9,6 @@ use LucasTrone\Main;
 
 class BlockBreakListener implements Listener {
 
-    /** @var Main $plugin */
     private $plugin;
 
     public function __construct(Main $plugin) {
@@ -24,15 +23,24 @@ class BlockBreakListener implements Listener {
             $block = $event->getBlock();
             $pos = $block->getPosition();
 
-            if (!isset($this->plugin->getZone()["point1"])) {
+            $zone = $this->plugin->getZone();
+            $player->sendMessage("§eDebug: Zone avant modification: " . print_r($zone, true));
+
+            if (!isset($zone["point1"])) {
                 $this->plugin->setZonePoint(1, $pos->getX(), $pos->getY(), $pos->getZ());
                 $player->sendMessage("§aPoint 1 défini à " . $pos->getX() . ", " . $pos->getY() . ", " . $pos->getZ());
                 $player->sendMessage("§aTapez un autre bloc pour définir le point 2.");
+
+                $zone = $this->plugin->getZone();
+                $player->sendMessage("§eDebug: Zone après définition du point 1: " . print_r($zone, true));
             } else {
                 $this->plugin->setZonePoint(2, $pos->getX(), $pos->getY(), $pos->getZ());
                 $player->sendMessage("§aPoint 2 défini à " . $pos->getX() . ", " . $pos->getY() . ", " . $pos->getZ());
                 $player->sendPopup("§aLa zone du trone est set.");
                 $this->plugin->removeSelectingPlayer($player);
+
+                $zone = $this->plugin->getZone();
+                $player->sendMessage("§eDebug: Zone après définition du point 2: " . print_r($zone, true));
             }
         }
     }
